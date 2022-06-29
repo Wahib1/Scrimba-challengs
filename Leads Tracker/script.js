e269edf3f9b792aba49bf01ef79f307d01a7c64a
Wahib1 <wquais212@gmail.com>
2022-06-29 23:48:04 +0530

    Saving current tab url feature added

--- Leads Tracker/script.js
+++ Leads Tracker/script.js
@@ -1,37 +1,51 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const delBtn = document.getElementById("delete-btn")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const delBtn = document.getElementById("delete-btn");
+const saveTabBtn = document.getElementById("saveTab-btn");
 
-delBtn.addEventListener("click", () => {
-  localStorage.clear()
-  myLeads = []
-  renderLeads()
-})
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+//getting leads from local storage
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+//funcationality to render only if something saved in local storage
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  renderLeads()
+  myLeads = leadsFromLocalStorage;
+  renderLeads(myLeads);
 }
-inputBtn.addEventListener("click", function() {
-  myLeads.push(inputEl.value)
-  inputEl.value = ""
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-  renderLeads()
-})
-
-function renderLeads() {
+//saving the url
+saveTabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      myLeads.push(tabs[0].url);
+      localStorage.setItem("myLeads", JSON.stringify(myLeads));
+      renderLeads(myLeads);
+    });
+  });
+});
+//function for saving input value
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads(myLeads);
+});
+//delete functionality
+delBtn.addEventListener("click", () => {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads(myLeads);
+});
+//rendering links as list in page
+function renderLeads(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
           <li>
-              <a target='_blank' href='${myLeads[i]}'>
-                  ${myLeads[i]}
+              <a target='_blank' href='${leads[i]}'>
+                  ${leads[i]}
               </a>
           </li>
       `;
   }
-  ulEl.innerHTML = listItems
+  ulEl.innerHTML = listItems;
 }
