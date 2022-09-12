29101c14340c77da14ad158e50243b4e54cb262f
Wahib1 <wquais212@gmail.com>
2022-09-12 13:03:25 +0530

    event on idicator added

--- Carousel/script.js
+++ Carousel/script.js
@@ -9,7 +9,37 @@ document
 document
   .getElementById("carousel-button-prev")
   .addEventListener("click", moveToPrevSlide);
-
+for(let slideIndic of slideIndicator){
+  slideIndic.addEventListener('click', ()=>{
+    moveToNextSlide()
+    if(slideIndic == slideIndicator[0]){
+      slides[0].classList.add("carousel-item-visible")
+      slides[1].classList.remove("carousel-item-visible")
+      slides[2].classList.remove("carousel-item-visible")
+      slideIndicator[0].classList.add("active-slide")
+      slideIndicator[1].classList.remove("active-slide")
+      slideIndicator[2].classList.remove("active-slide")
+      
+    }
+    if(slideIndic == slideIndicator[1]){
+      slides[1].classList.add("carousel-item-visible")
+      slides[0].classList.remove("carousel-item-visible")
+      slides[2].classList.remove("carousel-item-visible")
+      slideIndicator[1].classList.add("active-slide")
+      slideIndicator[0].classList.remove("active-slide")
+      slideIndicator[2].classList.remove("active-slide")
+    }
+    if(slideIndic == slideIndicator[2]){
+      slides[2].classList.add("carousel-item-visible")
+      slides[1].classList.remove("carousel-item-visible")
+      slides[0].classList.remove("carousel-item-visible")
+      slideIndicator[2].classList.add("active-slide")
+      slideIndicator[1].classList.remove("active-slide")
+      slideIndicator[0].classList.remove("active-slide")
+    }
+  })
+}
+setInterval(moveToNextSlide, 3000);
 function hideAllSlides() {
   for (let slide of slides) {
     slide.classList.remove("carousel-item-visible");
@@ -19,7 +49,7 @@ function hideAllSlides() {
     slideIndic.classList.remove("active-slide")
   }
 }
-setInterval(moveToNextSlide, 3000);
+
 function moveToNextSlide() {
   hideAllSlides();
 
